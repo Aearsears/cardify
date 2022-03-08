d37e217d8c88789366d364698f34f4f71128b2a7
Aearsears <53102185+Aearsears@users.noreply.github.com>
2022-03-08 00:06:16 -0500

    setup linter, formatter. created question and answer model in cards

--- cardify/settings.py
+++ cardify/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+from django.utils.encoding import force_str
+import django
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -31,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'cards.apps.CardsConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -105,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/New_York'
 
 USE_I18N = True
 
@@ -123,10 +126,8 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 GRAPHENE = {
-    'SCHEMA': 'app.schema.schema' # Where your Graphene schema lives
+    'SCHEMA': 'app.schema.schema'  # Where your Graphene schema lives
 }
 # When the changes is merged and released, remove the bottom code
 # https://github.com/graphql-python/graphene-django/pull/1275
-import django
-from django.utils.encoding import force_str
 django.utils.encoding.force_text = force_str
