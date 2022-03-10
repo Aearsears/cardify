d4c159b11a01910adb5399fe3cd1c57e4ec41781
Aearsears <53102185+Aearsears@users.noreply.github.com>
2022-03-09 23:46:26 -0500

    moved to psql for dev through docker containers

--- cardify/settings.py
+++ cardify/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'cardify.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
