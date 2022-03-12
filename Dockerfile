52e9f8cc80b749bb5fc19c5f142ffe3c7050f40e
Aearsears <53102185+Aearsears@users.noreply.github.com>
2022-03-11 19:03:28 -0500

    first v1 of dockerfile

---
+++ Dockerfile
@@ -0,0 +1,35 @@
+FROM ubuntu:18.04
+
+# Install dependencies
+RUN apt-get update && \
+    apt-get install -y --no-install-recommends python3 python3-pip python3-setuptools python3-dev git g++ build-essential cmake pkg-config wget vim
+
+WORKDIR /src/jupyter
+
+# Install all Python packages
+RUN pip3 install -U pip setuptools wheel
+RUN pip3 install setuptools_rust Cython
+RUN pip3 install git+https://github.com/Aearsears/Questgen.ai.git
+RUN pip3 install git+https://github.com/Aearsears/pke
+RUN pip3 install -U spacy=2.3.0  
+RUN python3 -m nltk.downloader popular
+RUN python3 -m nltk.downloader brown
+RUN python3 -m nltk.downloader universal_tagset
+RUN python3 -m nltk.downloader stopwords
+RUN python3 -m spacy download en_core_web_sm 
+
+# Download and unpack sense2vec model
+RUN wget https://github.com/explosion/sense2vec/releases/download/v1.0.0/s2v_reddit_2015_md.tar.gz
+RUN tar -xvf  s2v_reddit_2015_md.tar.gz
+
+# Install Jupyter notebook
+RUN pip3 install jupyter
+
+# Download and set `tini` as entrypoint (see https://github.com/krallin/tini for reasons)
+ENV TINI_VERSION v0.6.0
+ADD https://github.com/krallin/tini/releases/download/${TINI_VERSION}/tini /usr/bin/tini
+RUN chmod +x /usr/bin/tini
+ENTRYPOINT ["/usr/bin/tini", "--"]
+
+# Execute Jupyter notebook
+CMD ["jupyter", "notebook", "--port=8888", "--no-browser", "--ip=0.0.0.0", "--allow-root"] 
--- translator/translator.py
+++ translator/translator.py
@@ -1,60 +1,14 @@
+from Questgen import main
+from pprint import pprint
 import nltk
-import numpy as np
-from nltk.stem.wordnet import WordNetLemmatizer
-from nltk import pos_tag
-
+nltk.download('stopwords')
 # for help on the tags nltk.help.upenn_tagset('RB')
-# 'There will be a book here.'
+# python -m nltk.downloader universal_tagset
+# python -m spacy download en
 
-sentence = "there will be a book here."
-sentence = sentence.lower()
-sentence = sentence.replace('.', '')
-question_keywords = ['is', 'will', 'are', 'was', 'were', 'am']
-
-word_decomp = sentence.split(' ')
-
-for i in range(len(word_decomp)):
-    if word_decomp[i] in question_keywords:
-        for x in question_keywords:
-            if word_decomp.count(x) >> 1:
-
-                if pos_tag(word_decomp)[i+1][1] == 'VBN':
-                    word_decomp.insert(0, word_decomp[i])
-                    word_decomp.pop(i+1)
-                break
-            else:
-                word_decomp.insert(0, word_decomp[i])
-                word_decomp.pop(i+1)
-                break
-    else:
-        if i == len(word_decomp) - 2:
-            if word_decomp == sentence.split(' '):  # If sentence unchanged
-                # Verb reduction:
-                for i in range(len(word_decomp)):
-                    if 'VB' in pos_tag(word_decomp)[i][1]:
-                        # print "BINGO"
-                        word_decomp[i] = WordNetLemmatizer(
-                        ).lemmatize(word_decomp[i], 'v')
-                        # print "Verb reduction to present tense is required:"
-                word_decomp.insert(0, 'Did')
-
-for i in range(len(word_decomp)):  # Cycle through all words
-    # print str(np.array(pos_tag([word_decomp[i]]))[0][1])
-    # print [word_decomp[i]]
-    # Searches for a proper noun
-    if 'NP' in str(np.array(pos_tag([word_decomp[i]]))[0][1]):
-        # Capitalizes the proper noun
-        word_decomp[i] = word_decomp[i].capitalize()
-
-# Deletes . at end of sentence if it is there
-dot_to_que = word_decomp[len(word_decomp)-1].replace('.', '')
-# Capitalizes first word of sentence
-word_decomp.insert(0, word_decomp[0].capitalize())
-# Removes the possibly lowercase first word since the capitalized version has been inserted already
-word_decomp.pop(1)
-# Create a string to reform the question instead of word by word
-question = (' ').join(word_decomp)
-if '?' not in question:  # Ensures that ? is attached to end of the sentence
-    question = question + '?'
-
-print(question)  # Return the final formed question version of the input sentence
+qe = main.BoolQGen()
+payload = {
+    "input_text": "Sachin Ramesh Tendulkar is a former international cricketer from India and a former captain of the Indian national team. He is widely regarded as one of the greatest batsmen in the history of cricket. He is the highest run scorer of all time in International cricket."
+}
+output = qe.predict_boolq(payload)
+pprint(output)
