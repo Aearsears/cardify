fb3fcd422062ca7fe06bc2ae24f3b3478729e0aa
Aearsears <53102185+Aearsears@users.noreply.github.com>
2022-05-09 23:32:58 -0400

    added new paths for streamlit

--- cards/translator.py
+++ cards/translator.py
@@ -1,47 +1,53 @@
 import requests
 import json
+import os
+import asyncio
+import urllib.parse
+from threading import Thread
+from pyppeteer import launch
+
+asyncio.set_event_loop(asyncio.new_event_loop())
+
+EXEC_PATH = os.environ.get(
+    "GOOGLE_CHROME_SHIM", None)
+
+ML_LINK = os.environ.get(
+    "ML_LINK", "https://share.streamlit.io/aearsears/streamlit-qa-generator/main?text=")
 
 
 def get_questions(text):
-    # change to env variable
-    url = 'http://localhost:8888/api/'
-    headers = {"Content-Type": "application/json"}
     # takes in JSON of {"text": "this is that. that is this.", "max_questions":int}
-    res = requests.post(url, data=text, headers=headers)
-    res.raise_for_status()
-    data = res.json()
+    new_thread = Thread(target=between_callback, args=(data,))
+    new_thread.start()
+    new_thread.join()
+    return True
+
+
+def between_callback(args):
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+
+    loop.run_until_complete(task(args))
+    loop.close()
+
+
+async def task(data):
     print(data)
-    if data['questions']:
-        return data['questions']
-    else:
-        return data
-
-
-def format_response(text):
-    """ 
-    {
-    "statement": "Sachin Ramesh Tendulkar is a former international cricketer from India and a former captain of the Indian national team. He is widely regarded as one of the greatest batsmen in the history of cricket. He is the highest run scorer of all time in International cricket.",
-    "questions": [
-        {
-            "Question": "What is Sachin Ramesh Tendulkar's career?",
-            "Answer": "cricketer",
-            "id": 1,
-            "context": "Sachin Ramesh Tendulkar is a former international cricketer from India and a former captain of the Indian national team."
-        },
-        {
-            "Question": "Where is Sachin Ramesh Tendulkar from?",
-            "Answer": "india",
-            "id": 2,
-            "context": "Sachin Ramesh Tendulkar is a former international cricketer from India and a former captain of the Indian national team."
-        },
-        {
-            "Question": "What is the best cricketer?",
-            "Answer": "batsmen",
-            "id": 3,
-            "context": "He is widely regarded as one of the greatest batsmen in the history of cricket."
-        }
-    ]
-
-    }
- """
-    return 0
+    mlLink = ML_LINK + \
+        urllib.parse.quote(data["text"])
+    browser = await launch(handleSIGINT=False,
+                           handleSIGTERM=False,
+                           handleSIGHUP=False,
+                           headless=True,
+                           executablePath=EXEC_PATH,
+                           autoClose=False,
+                           args=['--no-sandbox']
+                           )
+    page = await browser.newPage()
+    await page.goto(mlLink, waitUntil="networkidle0")
+    await page.content()
+    elementHandle = await page.waitForSelector('div#root>div>div>div>iframe')
+    frame = await elementHandle.contentFrame()
+    # wait until the desired box appears
+    await frame.waitForSelector('h1#status-code')
+    await browser.close()
--- cards/views.py
+++ cards/views.py
@@ -3,14 +3,17 @@ from django.http import HttpResponse, JsonResponse
 from .translator import get_questions
 import json
 
+# Create your views here.
+
 
 def index(request):
-    return JsonResponse({"res": "hello world!"})
+    return HttpResponse("<p>Hello</p>")
 
 
-def create(request):
-    res = get_questions(
-        json.dumps({"text": "AWS Glue is a serverless data integration service that makes it easy to discover, prepare, and combine data for analytics, machine learning, and application development. AWS Glue provides all the capabilities needed for data integration so that you can start analyzing your data and putting it to use in minutes instead of months. Data integration is the process of preparing and combining data for analytics, machine learning, and application development. It involves multiple tasks, such as discovering and extracting data from various sources; enriching, cleaning, normalizing, and combining data; and loading and organizing data in databases, data warehouses, and data lakes. These tasks are often handled by different types of users that each use different products. AWS Glue provides both visual and code-based interfaces to make data integration easier. Users can easily find and access data using the AWS Glue Data Catalog. Data engineers and ETL (extract, transform, and load) developers can visually create, run, and monitor ETL workflows with a few clicks in AWS Glue Studio. Data analysts and data scientists can use AWS Glue DataBrew to visually enrich, clean, and normalize data without writing code. With AWS Glue Elastic Views, application developers can use familiar Structured Query Language (SQL) to combine and replicate data across different data stores.", "max_questions": 10}))
-    return HttpResponse(res)
-
-# Create your views here.
+def qa(request):
+    if request.method == 'POST':
+        # put into redis cache and issue a message to the MQ.
+        request.body
+        return HttpResponse(status=200)
+    else:
+        return HttpResponse("<p>Qa are you doing here?</p>")
