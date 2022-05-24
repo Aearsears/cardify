a98af9eb6ec275c62e906103132b456689595063
Aearsears <53102185+Aearsears@users.noreply.github.com>
2022-05-23 23:25:01 -0400

    django channels implement

--- cardify/settings.py
+++ cardify/settings.py
@@ -81,7 +81,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'cardify.wsgi.application'
-ASGI_APPLICATION = "cardify.routing.application"
+ASGI_APPLICATION = "cardify.asgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
--- cards/routing.py
+++ cards/routing.py
@@ -3,5 +3,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/cards/$', consumers.ChatConsumer),
+    re_path(r'ws/cards/$', consumers.ChatConsumer.as_asgi()),
 ]
